76db675e827989adc3b4fa10a0254bbf6f30daea
Olga Dementey <dementeyolya@gmail.com>
2020-10-03 22:47:36 +0300

    feature: upload the solution to typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) return 0;
+    array.sort((a, b) => a - b);
+    return array[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) return 0;
+    array.sort((a, b) => a - b);
+    return array[array.length - 1];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) return 0;
+    let sum = array.reduce(function (a, b) {
+        return a + b;
+    });
+    return sum / array.length;
+};
